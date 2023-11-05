55359f3d7f9396609f9f82b5be66f0313a0415d5
Daniel Abdel <danielmalek101@gmail.com>
2023-11-05 10:47:40 -0300

    feat: server-side registration

--- next-app/src/app/register/page.tsx
+++ next-app/src/app/register/page.tsx
@@ -1,34 +1,66 @@
 import { Header } from "@/components/Header";
 import { LinkBtn } from "@/components/LinkBtn";
+import prisma from "@/db";
+import { Prisma, registration } from "@prisma/client";
+import { redirect } from "next/navigation";
 import React from "react";
+
+async function createRegistration(data: FormData) {
+  "use server";
+  // type clientResgistration = Omit<
+  //   registration,
+  //   "id" | "createdAt" | "updatedAt"
+  // >;
+  type ResModel = {
+    name: string | undefined | object;
+    email: string | undefined | object;
+    profession: string | undefined | object;
+    salary: number | undefined | object;
+  };
+  const name = data.get("nome")?.valueOf();
+  const profession = data.get("profissao")?.valueOf();
+  const email = data.get("email")?.valueOf();
+  const salary = Number(data.get("salario")?.valueOf());
+  const registro: ResModel = {
+    name,
+    email,
+    profession,
+    salary,
+  };
+
+  await prisma.registration.create({ data: registro });
+
+  console.log(registro);
+  redirect("/");
+}
 
 function Register() {
   return (
     <>
       <Header title="Registro" />
-      <form className="flex flex-col gap-3">
+      <form action={createRegistration} className="flex flex-col gap-3">
         <input
           className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
           placeholder="Nome"
-          id="nome"
+          name="nome"
           type="text"
         />
         <input
           className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
           placeholder="Email"
-          id="email"
+          name="email"
           type="text"
         />
         <input
           className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
           placeholder="Profissão"
-          id="profissao"
+          name="profissao"
           type="text"
         />
         <input
           className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
           placeholder="Salário"
-          id="salario"
+          name="salario"
           type="text"
         />
         <button
